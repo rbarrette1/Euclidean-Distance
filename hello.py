6f8882bea55fbd0eefa8b003dc0829d3b29cd495
Rachelle B <rachellebarrette@hotmail.com>
2021-05-02 17:41:44 -0400

    calculate euclidean distance of two points

--- hello.py
+++ hello.py
@@ -1,12 +1,20 @@
-class MyFirstClass:
-    pass
+import math
+class EuclideanDistance:
+    def __init__(self):
+        self.dist = 0
 
-a = MyFirstClass()
+    def calculate(self, p1, p2):    
+        self.dist = math.sqrt( ( (math.pow(p1.x-p2.x, 2)) + (math.pow(p1.y-p2.y, 2)) ) )
+        return "The Euclidean Distance between (" + str(p1.x) + ", " + str(p1.y) + ") and (" + str(p2.x) + ", " + str(p2.y) + ") is " + str(self.dist) + "!"
 
-print(a)
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-a.x = 10
-a.y = 100
-print(a.x, a.y)
+a = Point(10, 10)
+b = Point(5, 5)
 
-print(a)
+ec = EuclideanDistance()
+dist = ec.calculate(a,b)
+print(dist)
